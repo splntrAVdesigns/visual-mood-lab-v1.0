30dd9fa0e8448a0801e83eb3ff5fbc9605857306
splntrAVdesigns <251049049+splntrAVdesigns@users.noreply.github.com>
2026-08-08 10:25:26 -0500

    index 4

--- lib/storage/index.ts
+++ lib/storage/index.ts
@@ -77,6 +77,7 @@ class VercelBlobStorage implements StorageAdapter {
 
     const token = await generateClientTokenFromReadWriteToken({
       pathname,
+      access: 'public',
       validUntil: expiresAt,
       allowedContentTypes: [contentType],
     });
